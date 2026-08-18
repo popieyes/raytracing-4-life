232e7bbb82a6b9b84ad52ffc732c06663484dae8
Santy <161971086+popieyes@users.noreply.github.com>
2026-08-18 19:15:24 +0200

    Created utility classes and a simple ray tracing that tracks a soft blue gradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include "core/color.h"
+#include "core/ray.h"
+
+color ray_color(const ray& r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-a) *color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
+}
 
 int main() {
   
-  // Image properties
-  int img_width = 256, img_height = 256;
+  // Scene Configuration
+  auto aspect_ratio = 16.0 / 9.0;
+  int img_width = 400;
+
+  // Height is computed from the image width and aspect ratio so we always get a correct aspect ratio
+  int img_height = int(img_width / aspect_ratio);
+  img_height = (img_height < 1) ? 1 : img_height;
+
+  // Camera
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (double(img_width)/img_height);
+  auto camera_center = point3(0,0,0);
+
+  auto viewport_u = vec3(viewport_width, 0,0);
+  auto viewport_v = vec3(0, -viewport_height, 0);
+
+  auto pixel_delta_u = viewport_u / img_width;
+  auto pixel_delta_v = viewport_v / img_height;
+
+  auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+  auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+  // Render
 
   // Render in PPM format. An RGB image in PPM format follows the following structure:
   /*
@@ -20,21 +51,19 @@ int main() {
 
   std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
 
-  for (int j = 0; j < img_width; j++) {
+  for (int j = 0; j < img_height; j++) {
+    std::clog << "\rScanlines remaining: " << (img_height - j) << ' ' << std::flush;
     for (int i = 0; i < img_width; i++) {
-      // Normalize color values to values between 0.0 to 1.0. This is done by convention
-      auto r = double(i) / (img_width - 1);
-      auto g = double(j) / (img_height - 1);
-      auto b = 0.0f;
-      
-      // Recast the values to pixel values between 0-255
-      int ir = int(255.999 * r);
-      int ig = int (255.999 * g);
-      int ib = int(255.999 * b);
-      
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+      auto ray_direction = pixel_center - camera_center;
+      ray r(camera_center, ray_direction);
+
+      color pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
     }
   }
   
+  std::clog << "\rDone.               \n";
+
   return 0;
 }
