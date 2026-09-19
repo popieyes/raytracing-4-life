1d313514163ebc9a16da9477490eb23a603cd6b3
Santy <161971086+popieyes@users.noreply.github.com>
2026-09-19 16:18:50 +0200

    Hittable List class used to define a collection of hittables easily and define scene objects quickly. Hit Record class now takes into account whether a ray impacts from outside or inside of the geometry.

--- include/core/hittable.h
+++ include/core/hittable.h
@@ -1,15 +1,24 @@
-#pragma once 
+#pragma once
+#include "core/utils.h"
+#include "core/vec3.h"
 #include "core/ray.h"
+#include "core/interval.h"
 
 class hit_record {
     public:
         point3 p;
         vec3 normal;
         double t; // Root where the collision happened
+        bool front_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 class hittable {
 
 public:
     virtual ~hittable() = default;
-    virtual bool hit(const ray& ray, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+    virtual bool hit(const ray& ray, interval ray_t, hit_record& rec) const = 0;
 };
